746ab63e7892e875f6138fd3100d77c851897bf9
Teppupdas <110745544+Teppupdas@users.noreply.github.com>
2026-06-28 20:30:09 +0200

    v0.0.50
    zarzadzanie lokacjami ulepszone:
    - instancjonowanie ich na odpowiedniej pozycji
    - usuwanie niechciancyh lokacji
    - jesli lokacja juz istnieje to jej nie dodaje kolejny raz lub wiecej razy

--- location_manager.gd
+++ location_manager.gd
@@ -12,8 +12,22 @@ var LOCATION_REGISTRY = {
 
 func _on_player_trigger_detector_area_entered(area: Area3D) -> void:
 	print("Trigger detected: ", area.name)
-	
-	
+
+
+
+	#USUWANIE LOKACJI
+	if area.has_meta("remove_location"):
+		var locations_to_remove = area.get_meta("remove_location") as Array
+		
+		for location in locations_to_remove:
+			var location_node = get_node_or_null(location)
+			
+			if location_node:
+				location_node.queue_free()
+
+
+
+
 	# DODAWANIE LOKACJI
 	if area.has_meta("add_location"):
 		var locations_to_add = area.get_meta("add_location") as Dictionary
@@ -22,13 +36,15 @@ func _on_player_trigger_detector_area_entered(area: Area3D) -> void:
 			var spawn_offset = locations_to_add[location] as Vector3
 			
 			if LOCATION_REGISTRY.has(location):
+				
+				if get_node_or_null(location) != null:
+					continue
+				
 				var location_uid = LOCATION_REGISTRY[location]
 				
 				var loaded_scene = load(location_uid) as PackedScene
 				if loaded_scene:
 					var new_location = loaded_scene.instantiate()
 					add_child(new_location)
-				
-				
-				
-					#new_location.global_position = spawn_offset
+					
+					new_location.global_position = area.get_parent().global_position + spawn_offset
